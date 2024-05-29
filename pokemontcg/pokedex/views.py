ae13aba60f311143d92305d4e90e6b08d16a7789
Logan Fu <lfu40@stuy.edu>
2024-05-28 23:03:27 -0400

    worked on all django models, and an adding card mechanism for api

--- pokemontcg/pokedex/views.py
+++ pokemontcg/pokedex/views.py
@@ -1,3 +1,205 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+import json
+import http.client
+import requests
+
+from .models import Pokemon, Type, Trainer, Energy
 
 # Create your views here.
+def loadInitialData(request):
+    if Type.objects.count() > 0:
+
+        # Add code for message about initial data already loaded
+
+        print("Type data already loaded")
+        return redirect('pokedex:test')
+    else:
+        api_url = 'https://api.pokemontcg.io/v2/types'
+        response = requests.get(api_url)
+        types = response.json()
+        # print(types['data'])
+        for type_name in types['data']:
+            print(type_name)
+            current_type = Type(
+                name = type_name
+            )
+            current_type.save()
+
+        # Add some cards into the database
+        insertInitialCards()
+        
+        # Add code for message about initial data successfully loading
+
+        # print("Type data successfully loaded")
+        return redirect('pokedex:test')
+    
+def insertInitialCards():
+    api_url = 'https://api.pokemontcg.io/v2/cards?q=set.id:base1'
+    response = requests.get(api_url)
+    set_data = response.json()['data']
+    for card_data in set_data:
+        try:
+            if card_data['supertype'] == 'Pokémon':
+                conditions = card_data['tcgplayer']['prices']
+                highest_market = 0
+                for condition in conditions:
+                    current_market = card_data['tcgplayer']['prices'][condition]['market']
+                    if current_market > highest_market:
+                        highest_market = current_market
+
+                current_pokemon = Pokemon(
+                    card_id = card_data['id'],
+                    name = card_data['name'],
+                    rarity = card_data['rarity'],
+                    image = card_data['images']['large'],
+                    prices = card_data['tcgplayer'],
+                    highest_market_price = highest_market,
+                )
+                current_pokemon.save()
+
+                for type in card_data['types']:
+                    current_type = Type.objects.get(name=type)
+                    current_pokemon.types.add(current_type)
+                current_pokemon.save()
+
+            # Add trainer card data
+            elif card_data['supertype'] == 'Trainer':
+                conditions = card_data['tcgplayer']['prices']
+                highest_market = 0
+                for condition in conditions:
+                    current_market = card_data['tcgplayer']['prices'][condition]['market']
+                    if current_market > highest_market:
+                        highest_market = current_market
+
+                current_trainer = Trainer(
+                    card_id = card_data['id'],
+                    name = card_data['name'],
+                    rarity = card_data['rarity'],
+                    image = card_data['images']['large'],
+                    prices = card_data['tcgplayer'],
+                    highest_market_price = highest_market,
+                )
+                current_trainer.save()
+            
+            # Add energy card data
+            elif card_data['supertype'] == 'Energy':      
+                conditions = card_data['tcgplayer']['prices']
+                highest_market = 0
+                for condition in conditions:
+                    current_market = card_data['tcgplayer']['prices'][condition]['market']
+                    if current_market > highest_market:
+                        highest_market = current_market
+
+                current_energy = Energy(
+                    card_id = card_data['id'],
+                    name = card_data['name'],
+                    image = card_data['images']['large'],
+                    prices = card_data['tcgplayer'],
+                    highest_market_price = highest_market,
+                )
+                current_energy.save()
+        except KeyError:
+            # Add message about missing key from API (NOT MY FAULT BRO)
+            print("This card is missing data from API")
+
+def createPokemonCard(request, card_id):
+    if Type.objects.count() == 0:
+        # Add code for error message about needing to load initial data first
+        print("Please load initial data")
+        return redirect('pokedex:test')
+
+    api_url = 'https://api.pokemontcg.io/v2/cards/{f_cardID}'.format(f_cardID = card_id)
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        card_data = response.json()['data']
+        try:
+            # Add pokemon type card
+            if card_data['supertype'] == 'Pokémon':
+                if Pokemon.objects.filter(card_id=card_data['id']).exists():
+                    # Add code for error message about card already existing
+                    print("Pokemon card already exists")
+                    return redirect('pokedex:test')
+                
+                conditions = card_data['tcgplayer']['prices']
+                highest_market = 0
+                for condition in conditions:
+                    current_market = card_data['tcgplayer']['prices'][condition]['market']
+                    if current_market > highest_market:
+                        highest_market = current_market
+
+                current_pokemon = Pokemon(
+                    card_id = card_data['id'],
+                    name = card_data['name'],
+                    rarity = card_data['rarity'],
+                    image = card_data['images']['large'],
+                    prices = card_data['tcgplayer'],
+                    highest_market_price = highest_market,
+                )
+                current_pokemon.save()
+
+                for type in card_data['types']:
+                    current_type = Type.objects.get(name=type)
+                    current_pokemon.types.add(current_type)
+                current_pokemon.save()
+
+            # Add trainer card data
+            elif card_data['supertype'] == 'Trainer':
+                if Trainer.objects.filter(card_id=card_data['id']).exists():
+                    # Add code for error message about card already existing
+                    print("Pokemon card already exists")
+                    return redirect('pokedex:test')
+                
+                conditions = card_data['tcgplayer']['prices']
+                highest_market = 0
+                for condition in conditions:
+                    current_market = card_data['tcgplayer']['prices'][condition]['market']
+                    if current_market > highest_market:
+                        highest_market = current_market
+
+                current_trainer = Trainer(
+                    card_id = card_data['id'],
+                    name = card_data['name'],
+                    rarity = card_data['rarity'],
+                    image = card_data['images']['large'],
+                    prices = card_data['tcgplayer'],
+                    highest_market_price = highest_market,
+                )
+                current_trainer.save()
+            
+            # Add energy card data
+            elif card_data['supertype'] == 'Energy':
+                if Energy.objects.filter(card_id=card_data['id']).exists():
+                    # Add code for error message about card already existing
+                    print("Pokemon card already exists")
+                    return redirect('pokedex:test')
+                
+                conditions = card_data['tcgplayer']['prices']
+                highest_market = 0
+                for condition in conditions:
+                    current_market = card_data['tcgplayer']['prices'][condition]['market']
+                    if current_market > highest_market:
+                        highest_market = current_market
+
+                current_energy = Energy(
+                    card_id = card_data['id'],
+                    name = card_data['name'],
+                    image = card_data['images']['large'],
+                    prices = card_data['tcgplayer'],
+                    highest_market_price = highest_market,
+                )
+                current_energy.save()
+        except KeyError:
+            # Add message about missing key from API (NOT MY FAULT BRO)
+            print("This card is missing data from API")
+
+
+
+
+        # print(card_data)
+
+    return redirect('pokedex:test')
+
+def testView(request):
+    return render(request, 'pokedex/test.html')
