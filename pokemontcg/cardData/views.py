d6e92bec0b6f7348a57c26b950d7fc45c9492ac5
Logan Fu <lfu40@stuy.edu>
2024-06-12 21:07:42 -0400

    formatted display card data template, small changes to code for displaying charts

--- pokemontcg/cardData/views.py
+++ pokemontcg/cardData/views.py
@@ -37,8 +37,10 @@ def showData(request):
     port = get_secret("database_port")
     password = get_secret("database_pwd")
     
+    connectString = 'dbname=\'{db_name}\' user=\'{db_user}\' host=\'{db_host}\' port=\'{db_port}\' password=\'{db_password}\''.format(db_name=dbname, db_user=user, db_host=host, db_port=int(port), db_password=password)
+    print(connectString)
     # Use variables to load df from database
-    engine = pg.connect("dbname='postgres' user='postgres' host='127.0.0.1' port='5432' password='4$g@38XUa2MvFb'")
+    engine = pg.connect(connectString)
     df_pokemon = pd.read_sql('select * from pokedex_pokemon', con=engine)
     df_trainer = pd.read_sql('select * from pokedex_trainer', con=engine)
     df_energy = pd.read_sql('select * from pokedex_energy', con=engine)
@@ -51,6 +53,8 @@ def showData(request):
     colors = ['#dd93ac', '#94e0e8', '#f8c471']
     fig1, ax1 = plt.subplots(figsize=(6, 6))
     ax1.pie(sizes, labels=labels, autopct='%1.0f%%', colors=colors)
+    plt.title('Percentage of cards based on supertype')
+
     buf = io.BytesIO()
     fig1.savefig(buf, format='png')
     buf.seek(0)
@@ -66,8 +70,10 @@ def showData(request):
     df_type_count
     fig2, ax2 = plt.subplots(figsize=(6, 6))
     labels = df_type_count['name_type']
-    colors = ['#8f8f8f', '#96795a', '#e6975c', '#338039', '#f5e662', '#9265a8', '#5581d4']
-    ax2.pie(df_type_count['id'], autopct='%1.0f%%', labels=labels, colors=colors)
+    # colors = ['#8f8f8f', '#96795a', '#e6975c', '#338039', '#f5e662', '#9265a8', '#5581d4']
+    ax2.pie(df_type_count['id'], autopct='%1.0f%%', labels=labels)
+    plt.title('Percentage of cards based on type')
+
     buf = io.BytesIO()
     fig2.savefig(buf, format='png')
     buf.seek(0)
@@ -78,8 +84,10 @@ def showData(request):
     df_rarity = df_pokemon.groupby(['rarity'], as_index=False).count()
     fig3, ax3 = plt.subplots(figsize=(6, 6))
     labels = df_rarity['rarity']
-    colors = ['#8c8c8c', '#5399cf', '#d9b92b', '#41a363']
-    ax3.pie(df_rarity['id'], autopct='%1.0f%%', labels=labels, colors=colors)
+    # colors = ['#8c8c8c', '#5399cf', '#d9b92b', '#41a363']
+    ax3.pie(df_rarity['id'], autopct='%1.0f%%', labels=labels, labeldistance=1.2)
+    plt.title('Percentage of cards based on rarity')
+
     buf = io.BytesIO()
     fig3.savefig(buf, format='png')
     buf.seek(0)
@@ -94,9 +102,9 @@ def showData(request):
     Energy = len(df_energy.index)
 
     sizes = [Pokemon, Trainer, Energy]
-    names = ['pokemon','trainer', 'energy']
+    names = ['Pokemon','Trainer', 'Energy']
     colors = ['#dd93ac', '#94e0e8', '#f8c471']
-    fig4, ax4 = plt.subplots(figsize=(6, 6))
+    fig4, ax4 = plt.subplots(figsize=(16, 6))
     ax4.bar(names, sizes, color=colors)
     plt.title('Number of Pokemon Cards')
     plt.xlabel('Type of Card')
@@ -114,11 +122,11 @@ def showData(request):
     df_pokemon_types = pd.merge(df_pokemon_types, df_types, left_on='type_id', right_on='id', suffixes=[None, '_type']).drop(['id_type'], axis=1)
     df_type_count = df_pokemon_types.groupby(['name_type'], as_index=False).count()
     df_type_count
-    fig5, ax5 = plt.subplots(figsize=(6, 6))
+    fig5, ax5 = plt.subplots(figsize=(16, 6))
     labels = df_type_count['name_type']
-    colors = ['#8f8f8f', '#96795a', '#e6975c', '#338039', '#f5e662', '#9265a8', '#5581d4']
+    # colors = ['#8f8f8f', '#96795a', '#e6975c', '#338039', '#f5e662', '#9265a8', '#5581d4']
 
-    ax5.bar(labels, df_type_count['id'], color = colors)
+    ax5.bar(labels, df_type_count['id'])
     plt.title('Number of Cards of Each Type')
     plt.xlabel('Types')
     plt.ylabel('Number of Cards')
@@ -134,10 +142,12 @@ def showData(request):
 
     trainer_r = trainer_r.reindex(pokemon_r.index, fill_value=0)
     result = pokemon_r + trainer_r
-    colors = ['#8c8c8c', '#5399cf', '#d9b92b', '#41a363']
-    rarities = ['Common', 'Rare', 'Rare Holo', 'Uncommon']
-    fig6, ax6 = plt.subplots(figsize=(6, 6))
-    ax6.bar(rarities, result, color=colors)
+    # colors = ['#8c8c8c', '#5399cf', '#d9b92b', '#41a363']
+    # rarities = ['Common', 'Rare', 'Rare Holo', 'Uncommon']
+    df_rarity = df_pokemon.groupby(['rarity'], as_index=False).count()
+    rarities = df_rarity['rarity']
+    fig6, ax6 = plt.subplots(figsize=(16, 6))
+    ax6.bar(rarities, result)
     plt.title('Number of Cards in Each Rarity')
     plt.xlabel('Rarities')
     plt.ylabel('Number of Cards')
